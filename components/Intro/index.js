6533ee905a6447e7d7a47a91ebecb140d3ebb289
this-shahriar <this.parash@gmail.com>
2020-11-10 02:39:30 +0600

    connects added

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -1,13 +1,23 @@
 import { Col, Divider, Row } from "antd";
 import styles from "./intro.module.scss";
-import { EnvironmentOutlined } from "@ant-design/icons";
+import {
+  EnvironmentOutlined,
+  GithubOutlined,
+  BehanceOutlined,
+  LinkedinOutlined,
+  Html5Outlined,
+  GoogleOutlined,
+} from "@ant-design/icons";
 
 export const Intro = () => {
   return (
     <div className={styles["intro-wrapper"]}>
       <Row>
-        <Col lg={18} md={18} xs={24}>
-          <h1>Shahriar Hasan Parash</h1>
+        <Col lg={12} md={18} xs={24}>
+          <div className={styles["hello-world"]}>
+            <h5>{`> Hello world`}</h5>
+          </div>
+          <h1>Luke Skywalker</h1>
           <h4>Front-end developer | Visual designer</h4>
           <h5>
             <EnvironmentOutlined /> <span>Dhaka, Bangladesh</span>
@@ -17,15 +27,53 @@ export const Intro = () => {
             <h4>About </h4>
             <Divider style={{ borderTop: "2px solid #2c2c2c" }} />
             <p>
-              I started my career with ES6 and ReactJS in front end development.
-              After 2 years of professional experience I am now comfortable with
-              React, Angular, ES6, TypeScript, Redux, RxJS and worked with
-              backend systems providing REST API and GraphQL. Completed BSc in
-              Computer Science and additionally courses at Free Code Camp
-              contains 300 hours of course materials. Fast and responsive UX to
-              the customer, maintainable code and learning at every level is my
-              motto.
+              Current front-end stack includes React, Angular, Next, ES6,
+              TypeScript, Redux, RxJS. Going all in for user experience, speed
+              and reliability.
             </p>
+          </div>
+          <div className="space" />
+          <div>
+            <h4>Connect</h4>
+            <Divider style={{ borderTop: "2px solid #2c2c2c" }} />
+            <div className={styles["connect-wrapper"]}>
+              <GithubOutlined
+                onClick={() =>
+                  window.open("https://github.com/this-shahriar", "_blank")
+                }
+                className={styles["connect"]}
+              />
+              <BehanceOutlined
+                onClick={() =>
+                  window.open(
+                    "https://www.behance.net/shahriarha4973",
+                    "_blank"
+                  )
+                }
+                className={styles["connect"]}
+              />
+              <LinkedinOutlined
+                onClick={() =>
+                  window.open("https://www.linkedin.com/in/shpr", "_blank")
+                }
+                className={styles["connect"]}
+              />
+              <Html5Outlined
+                onClick={() =>
+                  window.open(
+                    "https://stackoverflow.com/users/5352204/shahriar-hasan",
+                    "_blank"
+                  )
+                }
+                className={styles["connect"]}
+              />
+              <GoogleOutlined
+                onClick={() =>
+                  window.open("mailto:shahriarhasan0@gmail.com", "_blank")
+                }
+                className={styles["connect"]}
+              />
+            </div>
           </div>
         </Col>
       </Row>
