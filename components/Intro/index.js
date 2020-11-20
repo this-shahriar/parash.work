d9e8fe142865578f25bec46106c243416041f39e
this-shahriar <this.parash@gmail.com>
2020-11-21 03:48:58 +0600

    cleaning

--- components/Intro/index.js
+++ components/Intro/index.js
@@ -1,4 +1,4 @@
-import { Col, Divider, Row } from "antd";
+import { Button, Divider } from "antd";
 import styles from "./intro.module.scss";
 import {
   EnvironmentOutlined,
@@ -11,77 +11,55 @@ import {
 
 export const Intro = () => {
   return (
-    <div className={styles["intro-wrapper"]}>
-      <Row>
-        <Col lg={12} md={18} xs={24}>
-          <div className={styles["hello-world"]}>
-            <h5>{`> Hello world`}</h5>
-          </div>
-          <h1>Shahriar Hasan Parash</h1>
-          <h4>Front-end developer | Visual designer</h4>
-          <h5>
-            <EnvironmentOutlined /> <span>Dhaka, Bangladesh</span>
-          </h5>
-          <div className="space" />
-          <div className={styles["about-section"]}>
-            <h4>About </h4>
-            <Divider style={{ borderTop: "2px solid #2c2c2c" }} />
-            <p>
-              Current front-end stack includes React, Angular, Next, ES6,
-              TypeScript, Redux, RxJS. Going all in for user experience, speed
-              and reliability.
-            </p>
-          </div>
-          <div className="space" />
-          <div>
-            <h4>Connect</h4>
-            <Divider style={{ borderTop: "2px solid #2c2c2c" }} />
-            <div className={styles["connect-wrapper"]}>
-              <GithubOutlined
-                onClick={() =>
-                  window.open("https://github.com/this-shahriar", "_blank")
-                }
-                className={styles["connect"]}
-              />
-              <BehanceOutlined
-                onClick={() =>
-                  window.open(
-                    "https://www.behance.net/shahriarha4973",
-                    "_blank"
-                  )
-                }
-                className={styles["connect"]}
-              />
-              <LinkedinOutlined
-                onClick={() =>
-                  window.open("https://www.linkedin.com/in/shpr", "_blank")
-                }
-                className={styles["connect"]}
-              />
-              <Html5Outlined
-                onClick={() =>
-                  window.open(
-                    "https://stackoverflow.com/users/5352204/shahriar-hasan",
-                    "_blank"
-                  )
-                }
-                className={styles["connect"]}
-              />
-              <GoogleOutlined
-                onClick={() =>
-                  window.open("mailto:shahriarhasan0@gmail.com", "_blank")
-                }
-                className={styles["connect"]}
-              />
+    <div className="container">
+      <div className={styles.intro_wrapper}>
+        {Array.from({ length: 10 }, (item, idx) => {
+          return (
+            <div className={idx != 4 && styles.light}>
+              <h1>Shahriar Hasan Parash</h1>
+              <h4>Front-end Developer | Visual Designer</h4>
             </div>
+          );
+        })}
+        <div className="space" />
+        <div className={styles.connect_wrapper}>
+          <div className={styles.connects}>
+            <GithubOutlined
+              onClick={() =>
+                window.open("https://github.com/this-shahriar", "_blank")
+              }
+              className={styles["connect"]}
+            />
+            <BehanceOutlined
+              onClick={() =>
+                window.open("https://www.behance.net/shahriarha4973", "_blank")
+              }
+              className={styles["connect"]}
+            />
+            <LinkedinOutlined
+              onClick={() =>
+                window.open("https://www.linkedin.com/in/shpr", "_blank")
+              }
+              className={styles["connect"]}
+            />
+            <Html5Outlined
+              onClick={() =>
+                window.open(
+                  "https://stackoverflow.com/users/5352204/shahriar-hasan",
+                  "_blank"
+                )
+              }
+              className={styles["connect"]}
+            />
+            <GoogleOutlined
+              onClick={() =>
+                window.open("mailto:shahriarhasan0@gmail.com", "_blank")
+              }
+              className={styles["connect"]}
+            />
           </div>
-        </Col>
-        <Col lg={10} md={5} xs={0}>
-          <div className={styles["anim-svg"]}>
-            <object type="image/svg+xml" data="/static/anim.svg"></object>
-          </div>
-        </Col>
-      </Row>
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import { Header } from "../components/Header";
 import { Intro } from "../components/Intro";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="app">
-      {/* <Header /> */}
-      <div className="container">
-        <Intro />
+    <html>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Luke Sky Walker</title>
+      </Head>
+      <div className="app">
+        {/* <Header /> */}
+        <div className="container">
+          <Intro />
+        </div>
       </div>
-    </div>
+    </html>
   );
 }
